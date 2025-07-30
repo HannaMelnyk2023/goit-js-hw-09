49aa18806ecd97359b356401184940f9a036e8db
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-07-30 17:27:40 +0300

    1st done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let changeColor = null;
 
+startBtn.addEventListener("click", () => {
+    //заборона повторного кліку
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    changeColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+
+    }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(changeColor);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
