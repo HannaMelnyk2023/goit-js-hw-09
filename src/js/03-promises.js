5782d28029098436672863b39dbf358db2d597c0
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-08-06 17:58:34 +0300

    3d part start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+const form = document.querySelector("form");
+let delay = null;
+let step = null;
+let amount = null;
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  delay = Number(form.elements.delay.value);
+  step = Number(form.elements.step.value);
+  amount = Number(form.elements.amount.value)
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +16,13 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
+
+
 }
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
